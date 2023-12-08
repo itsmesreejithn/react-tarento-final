bf8407db40d04f8211ebda45f33de54cbb0259f2
Sreejith N <jithupreethy@gmail.com>
2023-12-08 21:02:13 +0530

    added password body

--- src/components/PasswordBody.jsx
+++ src/components/PasswordBody.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const PasswordBody = () => {
-  return <Container className="body-container">Passwords</Container>;
+  return <Container className="body-container"></Container>;
 };
 
 export default PasswordBody;
