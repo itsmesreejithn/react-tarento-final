d5e297e288c57300c43e969eb3011f27ec07d6bb
Sreejith N <jithupreethy@gmail.com>
2023-12-11 11:28:30 +0530

    added delete button

--- src/components/ListPasswrods.jsx
+++ src/components/ListPasswrods.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Card, Row, Col, Form } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import { useRecoilValue } from "recoil";
 import { passwordsAtom } from "../states/atoms";
 
 const ListPasswrods = () => {
   const passwordList = useRecoilValue(passwordsAtom);
+  const deletePassword = () => {};
   return (
     <>
       {passwordList.map((passwords, index) => (
@@ -17,6 +18,10 @@ const ListPasswrods = () => {
                 Email: {passwords.email}
                 <br />
                 Password: {passwords.password}
+                <br />
+                <Button variant="outline-danger" onClick={deletePassword}>
+                  Delete
+                </Button>
               </Card.Body>
             </Card>
           </Col>
